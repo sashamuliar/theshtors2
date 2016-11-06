84fde7cc5985cee40cde76301c34ceab025e8670
gogi <alexmason@ukr.net>
2016-11-06 19:12:18 +0200

    add to cart

--- js/script.js
+++ js/script.js
@@ -9,3 +9,13 @@ for (var i = 0; i < selectCaegories.length; i++){
 var clear = document.createElement("div");
 clear.className = "clear";
 categories.appendChild(clear);
+var cart = document.querySelectorAll( ".add_to_cart_button");
+var price = document.querySelectorAll(".price");
+for (var a = 0; a < cart.length; a++){
+	cart[a].onclick = function(){
+		this.className += " go_cart";
+		this.parentNode.querySelector(".price").className += " hide_price";
+		console.log("a");
+	};
+};
+console.log(cart);
